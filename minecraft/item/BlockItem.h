e3d4fee5ddd5a4cccde95ec314fea4d2976959a9
MCMrARM <mcmrarm@gmail.com>
2017-01-19 14:25:44 +0100

    Block, Material, BlockItem, Color

---
+++ minecraft/item/BlockItem.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class BlockItem : public Item {
+
+public:
+
+    virtual ~BlockItem();
+    virtual bool useOn(ItemInstance&, Entity&, int, int, int, signed char, float, float, float);
+    virtual std::string buildDescriptionName(ItemInstance const&) const;
+    virtual bool isEmissive(int) const;
+    virtual TextureUVCoordinateSet const& getIcon(int, int, bool) const;
+    virtual int getIconYOffset() const;
+
+    BlockItem(std::string const&, int);
+
+};
+
