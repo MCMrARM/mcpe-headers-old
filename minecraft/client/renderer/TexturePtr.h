ff093439c97b0930c4b90d8a64fd38ac6d8ed421
MCMrARM <mcmrarm@gmail.com>
2017-01-20 20:26:11 +0100

    Tessellator, ResourceLocation, ResourceFileSystem, TexturePtr, MaterialPtr, Mesh, MemoryTracker

---
+++ minecraft/client/renderer/TexturePtr.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include "../../util/ResourceLocation.h"
+
+namespace mce {
+
+struct Texture;
+struct TextureGroup;
+
+class TexturePtr {
+
+public:
+
+    TextureGroup* group;
+    Texture* texture;
+    ResourceLocation location;
+
+    TexturePtr();
+    TexturePtr(mce::TexturePtr&&);
+    TexturePtr(mce::TextureGroup&, ResourceLocation const&);
+
+    ~TexturePtr();
+
+    operator bool() const;
+    mce::TexturePtr& operator=(mce::TexturePtr&&);
+    mce::Texture* operator->();
+    mce::Texture const* operator->() const;
+    mce::Texture& operator*();
+    mce::Texture const& operator*() const;
+
+    mce::Texture* get() const;
+    mce::TextureGroup* getGroup() const;
+    int hashCode() const;
+
+    TexturePtr clone() const;
+    void _move(mce::TexturePtr&&);
+    void _deref();
+
+    void onGroupReloaded();
+    void onGroupDestroyed();
+
+    static TexturePtr makeTemporaryWrapper(mce::Texture&);
+
+    static TexturePtr NONE;
+
+};
+
+};
