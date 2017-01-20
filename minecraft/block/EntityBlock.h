bab9b3447f0ecf0b6c1cfe667899de2da95efb63
InusualZ <wesmoret98@gmail.com>
2017-01-20 09:35:31 -0500

    Added BlockEntity related headers

---
+++ minecraft/block/EntityBlock.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <memory>
+
+#include "Block.h"
+#include "entity/BlockEntity.h"
+
+class EntityBlock : public Block {
+
+public:
+    EntityBlock(std::string const&,int,Material const&);
+    
+    virtual ~EntityBlock() {}
+    virtual void neighborChanged(BlockSource &,BlockPos const&,BlockPos const&) const;
+    virtual void triggerEvent(BlockSource &,BlockPos const&,int,int) const;
+
+    std::unique_ptr<BlockEntity> newBlockEntity(BlockPos const&, BlockID) const;
+};
---
+++ minecraft/block/entity/BlockEntityFactory.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "BlockEntity.h"
+#include "../BlockID.h"
+
+class BlockEntityFactory {
+  
+public:
+    std::unique_ptr<BlockEntity> createBlockEntity(BlockEntityType, const BlockPos&, BlockID);
+};
