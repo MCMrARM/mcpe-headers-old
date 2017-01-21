95ffc4d824aa154c188525fecb3767648a458179
MCMrARM <mcmrarm@gmail.com>
2017-01-21 11:25:17 +0100

    Removed Hooks.h

--- minecraft/Hooks.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <tml/mod.h>
-#include <minecraft/gamemode/GameMode.h>
-
-#define UseItemHook(args...) TInstanceHook(bool, _ZN8GameMode9useItemOnER6PlayerP12ItemInstanceRK8BlockPosaRK4Vec3, GameMode, args)
